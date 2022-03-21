1995dd2cbda0bfd3aeba49c7c77ba57e06cec31f
KenyaPaiz <paizkenia5@gmail.com>
2022-03-21 17:55:12 -0600

    Update conexion.php

--- clases/conexion.php
+++ clases/conexion.php
@@ -2,7 +2,6 @@
     class Conexion{
         public $con;
         
-        //funcion para conectar
         public function conectar(){
             $this->con = mysqli_connect("localhost", "root", "", "proyect_tienda");
         }
