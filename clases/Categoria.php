01f0730c5970293fcca62821cc0148f11e854f52
Abraham Guerra <bramguerra2210@gmail.com>
2022-03-21 18:43:35 -0600

    Update Categoria.php

--- clases/Categoria.php
+++ clases/Categoria.php
@@ -7,8 +7,15 @@
 
      public function registrar (){
          $this->conectar();
-         if isset($_POST['categoria'])){
+         if (isset($_POST['categoria'])){
              $this->nombre = $_POST['categoria'];
+             if(isset($_POST['registrar'])){
+                 $query= "INSERT INTO categoria(nombre) VALUES ('$this->nombre')";
+                 $resultado = mysqli_query($this->con,$query);
+                 if(!empty($resultado)){
+                     echo "Se añadió categoría";
+                 } 
+             }
 
          }
      }
