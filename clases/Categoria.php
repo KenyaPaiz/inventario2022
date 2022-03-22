fa98fad864603f4a5a18a15fb35d930b7122671a
Abraham Guerra <bramguerra2210@gmail.com>
2022-03-21 18:24:25 -0600

    categoria

---
+++ clases/Categoria.php
@@ -0,0 +1,17 @@
+<?php
+ require('conexion.php');
+
+ class categoria extends Conexion{
+     public $categoriaid;
+     public $nombre;
+
+     public function registrar (){
+         $this->conectar();
+         if isset($_POST['categoria'])){
+             $this->nombre = $_POST['categoria'];
+
+         }
+     }
+ }
+
+?>
---
+++ vistas/categoria.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require('../clases/Categoria.php');
+        $categoria = new Categoria();
+    ?>
+    <form action="" method="POST">
+        <input type="text" name="categoria" placeholder="Ingrese el nombre de la categoria">
+        <input type="submit" name="registrar" value="Registrar categoria">
+    </form>
+    <?php $categoria->registrar(); ?>
+    <!--- Tabla de consultas -->
+    <br>
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Accion</th>
+        </thead>
+        <tbody>
+            <?php $categoria->consultar(); ?>
+        </tbody>
+    </table>
+</body>
+</html>
