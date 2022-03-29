ccd7a27004ae6a7b97f0a7a0cab51ebf4db16927
Abraham Guerra <bramguerra2210@gmail.com>
2022-03-28 19:50:14 -0600

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -15,7 +15,7 @@
 <center>
    
             
-            <form action="vistas/categoria.php" method="POST">
+            <form action="vistas/proveedor.php" method="POST">
             <img class="imagen" src="recursos/icono.jpg" alt="Logo TienditaShop" width = "60" height = "60">
             <h1>¿Qué deseas hacer?</h1>
             <input type="submit" value="ver Categoria">
@@ -23,8 +23,8 @@
             
             </form>
 
-<label for="">Marcas</label>
-    <a href="vistas/marca.php">Registrar Marcas</a>
+<!--<label for="">Marcas</label>
+    <a href="vistas/marca.php">Registrar Marcas</a> --> 
 
         
 </center>
@@ -32,7 +32,7 @@
 
 
 <center>
-
+            <form action="vistas/Marca.php" method="POST">
             <form method="POST">
             <i class="fa-solid fa-badge-check" class="icono"></i>
             <input type="submit" name="marca" value="Ver Marcas">
@@ -45,6 +45,7 @@
 
 
 <center>
+            <form action="vistas/Categoria.php" method="POST">
             <form method="POST">
             <i class="fa-solid fa-list-radio" class="icono"></i>
             <input type="submit" name="categoria" value="Ver Categorias">
