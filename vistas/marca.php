b13f2841a055247df6e8370a3ededaad7acbf127
Luis <luisenriquepanameno54@gmail.com>
2022-03-31 18:49:42 -0600

    NAV agregados

--- vistas/marca.php
+++ vistas/marca.php
@@ -8,7 +8,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Marca</title>
 </head>
-<body><br>
+<body>
+    <header>
+        <nav>
+            <a href="categoria.php">Categoria</a>
+            <a href="marca.php">Marca</a>
+            <a href="ver_producto.php">Producto</a>
+            <a href="proveedor.php">Proveedor</a>
+            <a href="registrar_producto.php">Registrar producto</a>
+        </nav>
+    </header>
     <div class="container">
         <?php
             require('../clases/Marca.php');
