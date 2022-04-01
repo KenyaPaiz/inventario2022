05fa71206fbf18a9018c8bf81f07ba96fa45a3e1
José Espinoza <Sortoespinoza22@gmail.com>
2022-03-31 18:35:56 -0600

    estilos

--- vistas/categoria.php
+++ vistas/categoria.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../recursos/estilo.categoria.css">
     <title>Categoria</title>
 </head>
 <body><br>
-    <div class="container">
+    <main class="container">
         <?php
             require('../clases/Categoria.php');
             $categoria = new Categoria();
@@ -32,6 +33,6 @@
                 <?php $categoria->consultar(); ?>
             </tbody>
         </table>
-    </div>
+    </main>
 </body>
 </html>
