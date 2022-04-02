421488417ea66079b65a81cf9f2b52685cc7cdb4
José Espinoza <Sortoespinoza22@gmail.com>
2022-04-01 21:06:19 -0600

    estilo proveedor

--- vistas/act_proveedor.php
+++ vistas/act_proveedor.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../recursos/css/estilo-nav.css">
+    <link rel="stylesheet" href="../recursos/css/estilo-act-proveedor.css">
     
     <title>Proveedores</title>
 </head>
@@ -38,19 +39,25 @@
             
         </nav>
     </header>
-    <main>
-    <?php 
-        require('../clases/proveedor.php');
-        $proveedor = new Proveedor(); 
-    ?>
-    <h1>Actualizar Proveedor</h1>
-        <form method="POST">
-            <?php $proveedor->obtenerId(); ?>
-            <div class="input-contenedor">
-                <input type="submit" name="actualizar" value="Actualizar Proveedor" class="button">
+    <center>
+        <main class="contenedor">
+            <?php 
+                require('../clases/proveedor.php');
+                $proveedor = new Proveedor(); 
+            ?>
+            <div class="h1-contenedor">
+                <h1>Actualizar Proveedor</h1>
             </div>
-        </form>
-    </main>
-    <?php $proveedor->actualizar(); ?>
+                <form method="POST">
+                    <div class="informacion">
+                        <?php $proveedor->obtenerId(); ?>
+                    </div>
+                    <div class="input-contenedor">
+                        <input type="submit" name="actualizar" class="btn btn-dark" value="Actualizar Proveedor" id="button">
+                    </div>
+                </form>
+        </main>
+        <?php $proveedor->actualizar(); ?>
+    </center>
 </body>
 </html>
